831a08c74c4187bffa5cd11d5aa42ec81010e1fc
18735442610 <583723781@qq.com>
2019-09-01 19:10:42 +0800

    9.1update

---
+++ app/src/main/java/com/mymusic/music/View/Activity/SignInActivity.java
@@ -0,0 +1,26 @@
+package com.mymusic.music.View.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.mymusic.music.R;
+import com.mymusic.music.base.BaseActivity;
+
+public class SignInActivity extends BaseActivity {
+
+
+    @Override
+    protected void initVariables(Intent intent) {
+
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_sign_in);
+    }
+
+    @Override
+    protected void LoadData() {
+
+    }
+}
